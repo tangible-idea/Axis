4c2e5172854907e99cbed831bc641b69294d5f08
thks10 <softinus@users.noreply.github.com>
2015-01-07 00:55:16 +0000

    -서브메뉴들 추가. -단일문제, 문제대전 fragment 추가.

--- src/com/softinus/axis/activities/MainActivity2.java
+++ src/com/softinus/axis/activities/MainActivity2.java
@@ -10,13 +10,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 import com.softinus.axis.fragments.FragmentMarket;
 import com.softinus.axis.fragments.FragmentStudy;
+import com.softinus.axis.fragments.QuestionCollectionFragment;
+import com.softinus.axis.fragments.QuestionSingleFragment;
 
 public class MainActivity2 extends BaseAxisAcivity implements OnClickListener
 {
 	private Button BTN_more, BTN_market, BTN_record, BTN_study;
+	private ToggleButton BTN_study_sub1, BTN_study_sub2, BTN_study_sub3;
+	private ToggleButton BTN_market_sub1, BTN_market_sub2;
+	
+	private ImageView IMG_sub;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -29,10 +37,26 @@ public class MainActivity2 extends BaseAxisAcivity implements OnClickListener
 		BTN_record= (Button) findViewById(R.id.btn_record);
 		BTN_study= (Button) findViewById(R.id.btn_study);
 		
+		BTN_study_sub1= (ToggleButton) findViewById(R.id.btn_study_sub1);
+		BTN_study_sub2= (ToggleButton) findViewById(R.id.btn_study_sub2);
+		BTN_study_sub3= (ToggleButton) findViewById(R.id.btn_study_sub3);
+		
+		BTN_market_sub1= (ToggleButton) findViewById(R.id.btn_market_sub1);
+		BTN_market_sub2= (ToggleButton) findViewById(R.id.btn_market_sub2);
+		
+		
+		
 		BTN_more.setOnClickListener(this);
 		BTN_market.setOnClickListener(this);
 		BTN_record.setOnClickListener(this);
 		BTN_study.setOnClickListener(this);
+		
+		BTN_study_sub1.setOnClickListener(this);
+		BTN_study_sub2.setOnClickListener(this);
+		BTN_study_sub3.setOnClickListener(this);
+		
+		BTN_market_sub1.setOnClickListener(this);
+		BTN_market_sub2.setOnClickListener(this);
 	}
 
 	@Override
@@ -73,6 +97,13 @@ public class MainActivity2 extends BaseAxisAcivity implements OnClickListener
 		}
 		else if(v.getId() == R.id.btn_market)
 		{
+			BTN_study_sub1.setVisibility(View.GONE);
+			BTN_study_sub2.setVisibility(View.GONE);
+			BTN_study_sub3.setVisibility(View.GONE);
+			
+			BTN_market_sub1.setVisibility(View.VISIBLE);
+			BTN_market_sub2.setVisibility(View.VISIBLE);
+			
 			Fragment fr;
 			fr= new FragmentMarket();
 			FragmentManager fm = getFragmentManager();
@@ -83,17 +114,67 @@ public class MainActivity2 extends BaseAxisAcivity implements OnClickListener
 		}
 		else if(v.getId() == R.id.btn_study)
 		{
+			BTN_study_sub1.setVisibility(View.VISIBLE);
+			BTN_study_sub2.setVisibility(View.VISIBLE);
+			BTN_study_sub3.setVisibility(View.VISIBLE);
+			
+			BTN_market_sub1.setVisibility(View.GONE);
+			BTN_market_sub2.setVisibility(View.GONE);
+			
+
+		}
+		else if(v.getId() == R.id.btn_record)
+		{
+			BTN_study_sub1.setVisibility(View.GONE);
+			BTN_study_sub2.setVisibility(View.GONE);
+			BTN_study_sub3.setVisibility(View.GONE);
+			
+			BTN_market_sub1.setVisibility(View.GONE);
+			BTN_market_sub2.setVisibility(View.GONE);
+		}
+		
+		else if(v.getId() == R.id.btn_study_sub1)
+		{
+			BTN_study_sub1.setChecked(true);
+			BTN_study_sub2.setChecked(false);
+			BTN_study_sub3.setChecked(false);
+			
 			Fragment fr;
-			fr= new FragmentStudy();
+			fr= new QuestionSingleFragment();
 			FragmentManager fm = getFragmentManager();
 			FragmentTransaction fragmentTransaction = fm.beginTransaction();
 			fragmentTransaction.replace(R.id.fragment_place, fr);
 			fragmentTransaction.commit();
 		}
-		else if(v.getId() == R.id.btn_record)
+		else if(v.getId() == R.id.btn_study_sub2)
 		{
+			BTN_study_sub1.setChecked(false);
+			BTN_study_sub2.setChecked(true);
+			BTN_study_sub3.setChecked(false);
 			
+			Fragment fr;
+			fr= new QuestionCollectionFragment();
+			FragmentManager fm = getFragmentManager();
+			FragmentTransaction fragmentTransaction = fm.beginTransaction();
+			fragmentTransaction.replace(R.id.fragment_place, fr);
+			fragmentTransaction.commit();
+		}
+		else if(v.getId() == R.id.btn_study_sub3)
+		{
+			BTN_study_sub1.setChecked(false);
+			BTN_study_sub2.setChecked(false);
+			BTN_study_sub3.setChecked(true);
 		}
 		
+		else if(v.getId() == R.id.btn_market_sub1)
+		{
+			BTN_market_sub1.setChecked(true);
+			BTN_market_sub2.setChecked(false);
+		}
+		else if(v.getId() == R.id.btn_market_sub2)
+		{
+			BTN_market_sub1.setChecked(false);
+			BTN_market_sub2.setChecked(true);
+		}
 	}
 }
